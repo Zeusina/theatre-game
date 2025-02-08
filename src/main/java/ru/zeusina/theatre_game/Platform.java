ba9981004d8a4a33536e29249e2d9fb910efb603
Zeusina <kachusov_k@outlook.com>
2025-02-08 18:58:04 +0300

    feat:✨ add Platform class for creating platforms in the game

---
+++ src/main/java/ru/zeusina/theatre_game/Platform.java
@@ -0,0 +1,9 @@
+package ru.zeusina.theatre_game;
+
+import java.awt.*;
+
+public class Platform extends Rectangle {
+    public Platform(int x, int y, int width, int height) {
+        setBounds(x, y, width, height);
+    }
+}
